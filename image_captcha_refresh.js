048c395dd374498dbdee43e28a7605032e9ce625
Dmitry Drozdik <dmitry.drozdik@gmail.com>
2012-09-26 10:45:24 +0300

    Issue #1779990 by DmitryDrozdik,AlexandrShalbetskiy : multiple captcha on one page.

--- image_captcha_refresh.js
+++ image_captcha_refresh.js
@@ -1,32 +1,29 @@
 (function ($) {
-
   Drupal.behaviors.imageCaptchaRefresh = {
     attach: function (context) {
-      var link = $('#reload-captcha');
-      if (link.length > 0) {
+      $('.reload-captcha', context).not('.processed').bind('click', function () {
+        $(this).addClass('processed');
+        var $form = $(this).parents('form');
+        // send post query for getting new captcha data
         var date = new Date();
-        link.not('.processed').bind('click', function () {
-          $(this).addClass('processed');
-          // send post query for getting new captcha data
-          var url = this.href + '?' + date.getTime();
-          $.get(
-            url,
-            {},
-            function (response) {
-              if(response.status == 1) {
-                $('.captcha').find('img').attr('src', response.data.url);
-                $('input[name=captcha_sid]').val(response.data.sid);
-                $('input[name=captcha_token]').val(response.data.token);
-              }
-              else {
-                alert(response.message);
-              }
-            },
-            'json'
-            );
-          return false;
-        });
-      }
+        var url = this.href + '?' + date.getTime();
+        $.get(
+          url,
+          {},
+          function (response) {
+            if(response.status == 1) {
+              $('.captcha', $form).find('img').attr('src', response.data.url);
+              $('input[name=captcha_sid]', $form).val(response.data.sid);
+              $('input[name=captcha_token]', $form).val(response.data.token);
+            }
+            else {
+              alert(response.message);
+            }
+          },
+          'json'
+          );
+        return false;
+      });
     }
   };
 })(jQuery);
